becbe2b3e2eefc6f7d466872d8172d23dd1040d5
Jacksonpirlo <jacksonflorezp@gmail.com>
2025-10-23 14:02:30 -0500

    feat(views): add logic and tailwind css of the view

---
+++ src/app/dashboard/home/page.tsx
@@ -0,0 +1,7 @@
+import HomePage from "@/components/template/Home"
+
+const Home = () => {
+    return(<HomePage/>)
+}
+
+export default Home
---
+++ src/app/dashboard/projects/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from "@/components/organism/Nav";
+import HomePage from "@/components/template/Home";
+
+export default function Home() {
+  return (
+    
+  );
+}
