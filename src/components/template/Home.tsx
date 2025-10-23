bb564b0cc19e6624016875456faa9371d0055dd2
Jacksonpirlo <jacksonflorezp@gmail.com>
2025-10-23 14:04:28 -0500

    feat(view): asdd principal view

---
+++ src/components/template/Home.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Image from "next/image"
+import MyImage from "@/assets/My-img8bit-com-Effect.gif"
+import Navbar from "../organism/Nav";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+
+const HomePage = () => {
+    const router = useRouter();
+    const pathname = usePathname()
+    console.log(pathname)
+    return(
+        <>
+        <div className="lg:hidden"><Navbar /></div>
+        <section className="p-6 mt-12  text-white">
+        <h1 className="font-bold text-4xl pirataOne">Portafolio</h1>
+            <article className="flex flex-col justify-center items-center m-3 mb-8">
+                <Image src={MyImage} alt="Photo" width={200} height={266} />
+                <h1 className="font-bold text-3xl mt-3 pirataOne">Sobre Mí</h1>
+                <p>Desarrollador Wed FullStak JR</p>
+            </article>
+
+            <article className="flex justify-center items-center max-w-730 text-center pl-[10] pr-[10]">
+                <p className="max-w-7xl pl-2 pr-2">
+                    Ya ha pasado más de un año desde que descubrí mi pasión por el desarrollo web. Desde entonces, cada día representa una nueva oportunidad para aprender, crecer profesionalmente y avanzar en mi desarrollo personal y económico. 
+                    Te invito a explorar mi portafolio; espero que lo disfrutes tanto como yo disfruto construirlo.
+                </p>
+            </article>
+        </section>
+            {pathname == "/home" || "/projects" || "/skills" || "languages" || "contact" ? <button onClick={() => {router.back()}} className="cursor-pointer font-sarpanch text-[24px] mb-4">Back</button> : ""}
+
+        </>
+    )
+}
+
+export default HomePage;
