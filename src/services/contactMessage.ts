873fd24f3e765ac6f207cc5f885f7e2c06afedd6
Jacksonpirlo <jacksonflorezp@gmail.com>
2025-10-27 14:32:37 -0500

    fix(cors): fix cors problems

--- src/services/contactMessage.ts
+++ src/services/contactMessage.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = "http://localhost:3000/api/contactMessage"
+const API = "https://portafolio-next-mocha.vercel.app/api/contactMessage"
 export const handleData = async (data: Record<string, unknown>) => {
     const res = await axios.post(API, data)
     return res.data
